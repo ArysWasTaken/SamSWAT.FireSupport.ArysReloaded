3e2f568e82e990e47feee749f40a1c38d862b2c7
Arys <arys.steam@gmail.com>
2025-11-09 21:41:04 +0700

    Added an extended CustomItemService to prevent adding items to handbook

--- project/SamSWAT.FireSupport.Server/ServerMod.cs
+++ project/SamSWAT.FireSupport.Server/ServerMod.cs
@@ -1,12 +1,11 @@
 using SPTarkov.DI.Annotations;
 using SPTarkov.Server.Core.DI;
 using SPTarkov.Server.Core.Helpers;
-using SPTarkov.Server.Core.Models.Common;
-using SPTarkov.Server.Core.Models.Spt.Config;
 using SPTarkov.Server.Core.Models.Spt.Mod;
-using SPTarkov.Server.Core.Servers;
-using SPTarkov.Server.Core.Services.Mod;
 using System.Reflection;
+using Path = System.IO.Path;
+using Range = SemanticVersioning.Range;
+using Version = SemanticVersioning.Version;
 
 namespace SamSWAT.FireSupport.ArysReloaded;
 
@@ -16,27 +15,18 @@ public record ServerModMetadata : AbstractModMetadata
 	public override string Name { get; init; } = "SamSWAT's FireSupport: Arys Reloaded";
 	public override string Author { get; init; } = "SamSWAT, Arys";
 	public override List<string>? Contributors { get; init; }
-	public override SemanticVersioning.Version Version { get; init; } = new(ModMetadata.VERSION);
-	public override SemanticVersioning.Range SptVersion { get; init; } = new($"~{ModMetadata.TARGET_SPT_VERSION}");
+	public override Version Version { get; init; } = new(ModMetadata.VERSION);
+	public override Range SptVersion { get; init; } = new($"~{ModMetadata.TARGET_SPT_VERSION}");
 	public override List<string>? Incompatibilities { get; init; }
-	public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; }
+	public override Dictionary<string, Range>? ModDependencies { get; init; }
 	public override string? Url { get; init; }
 	public override bool? IsBundleMod { get; init; }
 	public override string License { get; init; } = "Creative Commons BY-NC 3.0";
 }
 
 [Injectable(TypePriority = OnLoadOrder.PostDBModLoader + 1)]
-public class ServerMod(
-	ConfigServer configServer,
-	ModHelper modHelper,
-	CustomItemService customItemService)
-	: IOnLoad
+public class ServerMod(CustomItemServiceExtended customItemService, ModHelper modHelper) : IOnLoad
 {
-	private readonly ItemConfig _itemConfig = configServer.GetConfig<ItemConfig>();
-	private readonly TraderConfig _traderConfig = configServer.GetConfig<TraderConfig>();
-	private readonly RagfairConfig _ragfairConfig = configServer.GetConfig<RagfairConfig>();
-	private readonly ScavCaseConfig _scavCaseConfig = configServer.GetConfig<ScavCaseConfig>();
-
 	public Task OnLoad()
 	{
 		AddCustomItems();
@@ -54,36 +44,6 @@ public class ServerMod(
 		{
 			var newItemDetails = modHelper.GetJsonDataFromFile<NewCustomItemDetails>(pathToModDatabase, databaseFile);
 			customItemService.CreateItem(newItemDetails);
-			AddItemToBlacklists(newItemDetails);
-		}
-	}
-
-	private void AddItemToBlacklists(NewCustomItemDetails newItemDetails)
-	{
-		if (newItemDetails.BlacklistDetails == null) return;
-		
-		MongoId itemId = newItemDetails.NewItem!.Id;
-
-		if (!newItemDetails.BlacklistDetails.BlacklistGlobally)
-		{
-			if (newItemDetails.BlacklistDetails.BlacklistFromFence)
-			{
-				_traderConfig.Fence.Blacklist.Add(itemId);
-			}
-
-			if (newItemDetails.BlacklistDetails.BlacklistFromFlea)
-			{
-				_ragfairConfig.Dynamic.Blacklist.Custom.Add(itemId);
-			}
-
-			if (newItemDetails.BlacklistDetails.BlacklistFromScavCase)
-			{
-				_scavCaseConfig.RewardItemParentBlacklist.Add(itemId);
-			}
-		}
-		else
-		{
-			_itemConfig.Blacklist.Add(itemId);
 		}
 	}
 }
